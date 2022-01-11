9a7110b71a66bda7029b7200591598a882de7a15
l <l@DESKTOP-BEN1KAQ>
2022-01-11 09:06:00 -0300

    Teste Git Trabalho

--- exercicios/src/fundamentos/github.java
+++ exercicios/src/fundamentos/github.java
@@ -2,7 +2,7 @@ package fundamentos;
 
 public class github {
 public static void main(String[] args) {
-	String a = "Olá, Peter!";
+	String a = "Olá, Peter, estou no github no trabalho!";
 	System.out.println(a.toUpperCase());
 }
 }
